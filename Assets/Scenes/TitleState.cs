ce530d97e97b06e2c757a5af501893a5d00c6070
Edouard Philippe <edouard.philippe@onogone.com>
2020-03-07 12:48:30 +0100

    State Machine integration

---
+++ Assets/Scenes/TitleState.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleState : MonoBehaviour
+{
+    public TitleState() { 
+    
+    }
+
+    void Update() {
+        Debug.Log("HEHO");
+    }
+}
