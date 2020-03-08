bcfed3bbaf2f281888440f171687eb0d855710ef
Diana <dianaalves23@hotmail.com>
2020-03-08 13:06:15 +0100

    changes

---
+++ Assets/corkBoard.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class corkBoard : MonoBehaviour
+{
+    private UnityAction propListener;
+    public List<GameObject> corkboardObjects;
+
+    void Awake()
+    {
+        propListener = new UnityAction(TriggerAnimation);
+    }
+
+
+    void OnEnable()
+    {
+        EventManager.StartListening("cork", TriggerAnimation);
+    }
+
+    void OnDisable()
+    {
+        EventManager.StopListening("cork", TriggerAnimation);
+    }
+
+    private void TriggerAnimation()
+    {
+        if (corkboardObjects[0].activeInHierarchy)
+        {
+            corkboardObjects[0].SetActive(false);
+            corkboardObjects[1].SetActive(true);
+        }
+        else if (corkboardObjects[1].activeInHierarchy)
+        {
+            corkboardObjects[1].SetActive(false);
+            corkboardObjects[2].SetActive(true);
+        }
+
+        else if (corkboardObjects[2].activeInHierarchy)
+        {
+            corkboardObjects[2].SetActive(false);
+            corkboardObjects[3].SetActive(true);
+        }
+        else if (corkboardObjects[3].activeInHierarchy)
+        {
+            corkboardObjects[3].SetActive(false);
+            corkboardObjects[4].SetActive(true);
+        }
+    }
+}
---
+++ Assets/postiManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class postiManager : MonoBehaviour
+{
+    private UnityAction propListener;
+    public List<GameObject> postItText;
+
+    void Awake()
+    {
+        propListener = new UnityAction(TriggerAnimation);
+    }
+
+
+    void OnEnable()
+    {
+        EventManager.StartListening("postIt", TriggerAnimation);
+    }
+
+    void OnDisable()
+    {
+        EventManager.StopListening("postIt", TriggerAnimation);
+    }
+    private void TriggerAnimation()
+    {
+        if (postItText[0].activeInHierarchy)
+        {
+            postItText[0].SetActive(false);
+            postItText[1].SetActive(true);
+        }
+        else if (postItText[1].activeInHierarchy)
+        {
+            postItText[1].SetActive(false);
+            postItText[2].SetActive(true);
+        }
+
+        else if (postItText[2].activeInHierarchy)
+        {
+            postItText[2].SetActive(false);
+            postItText[3].SetActive(true);
+        }
+        else if (postItText[3].activeInHierarchy)
+        {
+            postItText[3].SetActive(false);
+            postItText[4].SetActive(true);
+        }
+    }
+}
