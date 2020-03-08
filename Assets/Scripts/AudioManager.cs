923ace5b4558698912ffda68f68d32de99d967a4
Diana <dianaalves23@hotmail.com>
2020-03-08 12:12:53 +0100

    no message

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AudioManager : MonoBehaviour
+{
+    private UnityAction endVotingListener;
+
+    void Awake() {
+        endVotingListener = new UnityAction(UpdateSound);
+    }
+
+    private void UpdateSound()
+    {
+        
+    }
+
+    void OnEnable()
+    {
+        EventManager.StartListening("EndVoting",endVotingListener);
+        
+    }
+
+    void OnDisable()
+    {
+        EventManager.StopListening("EndVoting", endVotingListener);
+        
+    }
+}
